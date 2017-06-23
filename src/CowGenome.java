ec33abd84910359346ed20c7a34d42d935c3e2fc
Young Park <young_park@brown.edu>
2017-06-23 12:18:53 +0900

    Added the input/output system for testing.

--- src/CowGenome.java
+++ src/CowGenome.java
@@ -1,4 +1,8 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Practice program - USACO Bronze #2
@@ -27,8 +31,47 @@ public class CowGenome {
        }
    }
 
+   public boolean getPotentialLocation() {
+       return this.potentialLocation;
+   }
+
    public static void main(String[] args) {
-       
+       for (int i = 0; i <= 10; i++) {
+           File input = new File(i + ".in");
+           try {
+               // variables
+               ArrayList<String> spottyCow = new ArrayList<String>();
+               ArrayList<String> plainCow = new ArrayList<String>();
+               Scanner sc = new Scanner(input);
+               int n = sc.nextInt();
+               int m = sc.nextInt();
+               String[] allCows = new String[n];
+               for (int j = 0; j < n; j++) {
+                   String temp = sc.nextLine();
+                   allCows[j] = temp;
+               }
+               int counter = 0;
+               // putting variables into instance
+               for (int l = 0; l < m; l++) {
+                   for (int t = 0; t < n/2; t++) {
+                      spottyCow.add(allCows[t].substring(l, l+1));
+                   }
+                   for (int p = n/2; p < n; p++) {
+                      plainCow.add(allCows[p].substring(l, l+1));
+                   }
+                   CowGenome w = new CowGenome(spottyCow, plainCow);
+                   if (w.getPotentialLocation()) {
+                       counter++;
+                   }
+               }
+               // output
+               PrintWriter pw = new PrintWriter(i + ".out");
+               pw.println(counter);
+               pw.close();
+           } catch (FileNotFoundException a) {
+               System.out.println("File not found!");
+           }
+       }
    }
 
 }
