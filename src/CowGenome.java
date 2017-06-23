95d4c42b5c020e20a1d8ca9c7893fc0c4782f863
Young Park <young_park@brown.edu>
2017-06-23 13:53:50 +0900

    Added the for loop that would go through all ten input cases. The program works as intended.

--- src/CowGenome.java
+++ src/CowGenome.java
@@ -31,51 +31,10 @@ public class CowGenome {
     }
 
     public static void main(String[] args) {
-        File input = new File("1.in");
-        try {
-            // variables
-            Scanner sc = new Scanner(input);
-            int n = sc.nextInt();
-            int m = sc.nextInt();
-            String[] allCows = new String[n*2];
-            for (int j = 0; j < allCows.length; j++) {
-                String temp = sc.next();
-                allCows[j] = temp;
-            }
-            int counter = 0;
-            // get the counter
-            for (int l = 0; l < m; l++) {
-                ArrayList<String> spottyCow = new ArrayList<String>();
-                ArrayList<String> plainCow = new ArrayList<String>();
-                for (int p = 0; p < 2*n; p++) {
-                    String temp = allCows[p].substring(l, l+1);
-                    if (p < n) {
-                        spottyCow.add(temp);
-                    } else if (p >= n) {
-                        plainCow.add(temp);
-                    }
-                }
-                CowGenome w = new CowGenome(spottyCow, plainCow);
-                if (w.possibleLocation) {
-                    counter++;
-                }
-            }
-            // output
-            PrintWriter pw = new PrintWriter("1.out");
-            pw.println(counter);
-            pw.close();
-        } catch (FileNotFoundException a) {
-            System.out.println("File not found!");
-        }
-    }
-
-    /*public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
             File input = new File(i + ".in");
             try {
                 // variables
-                ArrayList<String> spottyCow = new ArrayList<String>();
-                ArrayList<String> plainCow = new ArrayList<String>();
                 Scanner sc = new Scanner(input);
                 int n = sc.nextInt();
                 int m = sc.nextInt();
@@ -87,14 +46,18 @@ public class CowGenome {
                 int counter = 0;
                 // get the counter
                 for (int l = 0; l < m; l++) {
-                    for (int t = 0; t < n/2; t++) {
-                        spottyCow.add(allCows[t].substring(l, l+1));
-                    }
-                    for (int p = n/2; p < n; p++) {
-                        plainCow.add(allCows[p].substring(l, l+1));
+                    ArrayList<String> spottyCow = new ArrayList<String>();
+                    ArrayList<String> plainCow = new ArrayList<String>();
+                    for (int p = 0; p < 2*n; p++) {
+                        String temp = allCows[p].substring(l, l+1);
+                        if (p < n) {
+                            spottyCow.add(temp);
+                        } else if (p >= n) {
+                            plainCow.add(temp);
+                        }
                     }
                     CowGenome w = new CowGenome(spottyCow, plainCow);
-                    if (w.getPotentialLocation()) {
+                    if (w.possibleLocation) {
                         counter++;
                     }
                 }
@@ -102,10 +65,10 @@ public class CowGenome {
                 PrintWriter pw = new PrintWriter(i + ".out");
                 pw.println(counter);
                 pw.close();
-           } catch (FileNotFoundException a) {
-               System.out.println("File not found!");
-           }
-       }
-    }*/
+            } catch (FileNotFoundException a) {
+                System.out.println("File not found!");
+            }
+        }
+    }
 
 }
